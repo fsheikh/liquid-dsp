1e6a50156ed15a8fbde18c64eb6104eb6d6f39d4
Faheem Sheikh <fahim.sheikh@gmail.com>
2019-01-13 14:40:26 +0100

    sandbox: Sound level meter-IV
    Summary: High frequency set is now being properly detected.
    
    Signed-off-by: Faheem Sheikh

--- sandbox/sound_level_meter.c
+++ sandbox/sound_level_meter.c
@@ -22,7 +22,6 @@
 #define OCT_HALFBAND_FACTOR      1.1225f /** 2^1/6 */
 #define OCT_HALF_PI              M_PI * 0.5
 #define OCT_EDGE_FREQS           30
-#define OCT_NORM_PIVOT           1.0f/20.0f
 #define OCT_ERROR_CODE           -1
 #define OCT_CUBEROOT_TWO         (OCT_HALFBAND_FACTOR * OCT_HALFBAND_FACTOR) /** 2^1/3 */
 #define OCT_CUBEROOT_TWO_SQ      (OCT_CUBEROOT_TWO * OCT_CUBEROOT_TWO) /** 2^2/3 */
@@ -57,7 +56,7 @@ size_t get_sound_data(float* data_f, char* filename)
         if (fid != NULL) {
             samples_read = 0;
             while(samples_read <= MAX_INPUT_DATA_SIZE) {
-                if (fscanf(fid, "%f", &data_f[samples_read]) == EOF) {
+                if (fscanf(fid, "%e", &data_f[samples_read]) == EOF) {
                     fprintf(stderr, "Info: Finished parsing input file %s with samples %zu\n",
                             filename, samples_read);
                     break;
@@ -77,13 +76,14 @@ size_t get_sound_data(float* data_f, char* filename)
 }
 // n: Filter order
 // f0: Center frequency
+// fs: Sampling rate for which filter is designed.
 // b: Numerator coefficients of digital IIR filter, length 2*N+1
 // a: Denominator coefficient of digital IIR filter, length 2*N+1
 // returns -1 on Error
-int one_third_filter(unsigned int n, float f0, float* b, float* a) {
+int one_third_filter(unsigned int n, float f0, float fs, float* b, float* a) {
 
     // validate input
-    if (f0 <= 0 || f0 >= OCT_ULIMIT_CENTER_FREQ) {
+    if (f0 <= 0 || f0 >= OCT_ULIMIT_CENTER_FREQ * fs) {
         fprintf(stderr,"error: %12.4e, centre frequency out of range\n", f0);
         return OCT_ERROR_CODE;
     }
@@ -92,63 +92,22 @@ int one_third_filter(unsigned int n, float f0, float* b, float* a) {
         fprintf(stderr, "Invalid co-efficient's array supplied to one-third octave design function\n");
         return OCT_ERROR_CODE;
     }
-    // number of analaog poles/zeros
-    unsigned int npa = n;
-    unsigned int nza;
-
-    // analog poles/zeros/gain
-    float complex pa[n];
-    float complex za[n];
-    float complex ka;
-    float complex k0;
-
-    nza = 0;
-    k0 = 1.0f;
-    butter_azpkf(n,za,pa,&ka);
 
     // complex digital poles/zeros/gain
     // NOTE: allocated double the filter order to cover band-pass, band-stop cases
-    float complex zd[2*n];
-    float complex pd[2*n];
-    float complex kd;
     float f1 = f0 / OCT_HALFBAND_FACTOR;
     float f2 = f0 * OCT_HALFBAND_FACTOR;
     float Qr = f0 / (f2 - f1);
-    float Qd = Qr * (OCT_HALF_PI / n) / sin(OCT_HALF_PI / n);
+    float Qd = Qr * (OCT_HALF_PI / n) / sinf(OCT_HALF_PI / n);
     float alpha = ((1 + sqrtf(1 + (4 * powf(Qd, 2.0f)))) / 2.0f) / Qd;
     assert(alpha > 1.0f);
-    float fc = f0 * alpha;
-    float m = iirdes_freqprewarp(LIQUID_IIRDES_BANDPASS, 0.5f * fc , f0);
-    printf("m : %12.8f\n", m);
-    bilinear_zpkf(za,    nza,
-                  pa,    npa,
-                  k0,    m,
-                  zd, pd, &kd);
-
-    // transform zeros, poles in band-pass since octave 1/3
-    // should have bandpass characteristics
-    // allocate memory for transformed zeros, poles
-    float complex zd1[2*n];
-    float complex pd1[2*n];
-
-    // run zeros, poles trasform
-    iirdes_dzpk_lp2bp(zd, pd,   // low-pass prototype zeros, poles
-                      n,        // filter order
-                      f0,       // center frequency
-                      zd1,pd1); // transformed zeros, poles (length: 2*n)
-
-    // copy transformed zeros, poles
-    memmove(zd, zd1, 2*n*sizeof(float complex));
-    memmove(pd, pd1, 2*n*sizeof(float complex));
-
-    // convert complex digital poles/zeros/gain into transfer function
-    iirdes_dzpk2tff(zd,pd,2*n,kd,b,a);
-#if 0
-    // print coefficients
-    unsigned int i;
-    for (i=0; i<=2*n; i++) printf("a[%3u] = %12.8f;\n", i, a[i]);
-    for (i=0; i<=2*n; i++) printf("b[%3u] = %12.8f;\n", i, b[i]);
-#endif
+    printf("alpha=%12.4e fc=%12.4e f0=%12.4e f2=%12.4e\r\n", alpha, f0*alpha, f0, f2);
+    liquid_iirdes(LIQUID_IIRDES_BUTTER,
+                  LIQUID_IIRDES_BANDPASS,
+                  LIQUID_IIRDES_TF,
+                  n,
+                  alpha*(f0/fs), f0/fs, 60.0f, 1.0f,
+                  b, a);
     return 0;
 }
 
@@ -173,7 +132,7 @@ unsigned construct_freq_set(float* freq_set, unsigned* pivot, float sampling_rat
         fprintf(stderr, "Info! sampling rate not enough for processing whole frequeny range\n");
         f_index = 0;
         while (freq_set[f_index] < sampling_rate/3.0f) {
-            freq_set[f_index] = freq_set[f_index] / sampling_rate;
+            freq_set[f_index] = freq_set[f_index];
             f_index++ ;
         }
         if (f_index < OCT_EDGE_FREQS - 1) {
@@ -185,7 +144,7 @@ unsigned construct_freq_set(float* freq_set, unsigned* pivot, float sampling_rat
     // Find pivot frequency, below which octave filters will be applied
     // after decimation.
     unsigned p_index = 0;
-    while ((p_index < f_index) && (freq_set[p_index] < OCT_NORM_PIVOT)) {
+    while ((p_index < f_index) && (freq_set[p_index] < sampling_rate/20.0f)) {
         p_index++;
     }
     printf("final index %d, pivot index %d\n", f_index, p_index);
@@ -262,6 +221,7 @@ int main(int argc, char*argv[]) {
     fprintf(fid, "rms_power=NaN*ones(1,%d);\n", OCT_EDGE_FREQS);
     fprintf(fid, "freq_set=zeros(1,%d);\n", OCT_EDGE_FREQS);
     fprintf(fid, "num_samples=%lu;\n", data_read);
+    fprintf(fid, "XX=[%12.4e %12.4e %12.4e %12.4e %12.4e];\n", sound_data[9], sound_data[99], sound_data[999], sound_data[1999], sound_data[2999]);
     fprintf(fid, "Fs=%12.4e;\n", fs);
 
     float freq_set[OCT_EDGE_FREQS]; // Set of center frequencies derived from 1/3 octave band edges
@@ -288,7 +248,7 @@ int main(int argc, char*argv[]) {
         fprintf(fid, "b_%d=zeros(1, %d);\n", b_index+1, L);
         fprintf(fid, "a_%d=zeros(1, %d);\n", b_index+1, L);
 
-        if (one_third_filter(n, freq_set[b_index], b, a) == OCT_ERROR_CODE) {
+        if (one_third_filter(n, freq_set[b_index], fs, b, a) == OCT_ERROR_CODE) {
             fprintf(stderr, "Failed to design octave 1/3 filter for freq_set[%d]=%12.8e\n",
                     b_index, freq_set[b_index]);
             free(sound_data);
@@ -299,11 +259,19 @@ int main(int argc, char*argv[]) {
             fprintf(fid, "b_%d(%d)=%12.4e;\n", b_index+1, findex+1, b[findex]);
             fprintf(fid, "a_%d(%d)=%12.4e;\n", b_index+1, findex+1, a[findex]);
         }
+#if 0
         float accum_power = 0.0f;
         float filt_output[OCT_BLK_SIZE] = {0.0f};
         for (size_t data_index = 0; data_index < data_read; data_index+=OCT_BLK_SIZE) {
             iirfilt_rrrf_execute_block(octave_one_third, &sound_data[data_index], OCT_BLK_SIZE, filt_output);
             accum_power += liquid_sumsqf(filt_output, OCT_BLK_SIZE);
+        }
+#endif
+        float accum_power = 0.0f;
+        float filt_output = 0.0f;
+        for (size_t data_index = 0; data_index < data_read; data_index++) {
+            iirfilt_rrrf_execute(octave_one_third, sound_data[data_index], &filt_output);
+            accum_power += liquid_sumsqf(&filt_output, 1);
         }
         rms_power[b_index] = 10*log10f(accum_power/(float) data_read);
         fprintf(fid, "rms_power(%d)=%12.8f\n", b_index+1, rms_power[b_index]);
@@ -321,20 +289,22 @@ int main(int argc, char*argv[]) {
     float bl[2*n + 1];
     float al[2*n + 1];
     float half_fc = freq_set[p_index-1] * 2.0f;
-    if (one_third_filter(n, half_fc, bu, au) == OCT_ERROR_CODE) {
+    if (one_third_filter(n, half_fc, fs, bu, au) == OCT_ERROR_CODE) {
         fprintf(stderr, "Failed to design upper octave 1/3 filter for freq=%12.8e\n", half_fc);
         free(sound_data);
+        fclose(fid);
         exit(1);
     }
-    if (one_third_filter(n, half_fc/OCT_CUBEROOT_TWO, bc, ac) == OCT_ERROR_CODE) {
+    if (one_third_filter(n, half_fc/OCT_CUBEROOT_TWO, fs, bc, ac) == OCT_ERROR_CODE) {
         fprintf(stderr, "Failed to design center octave 1/3 filter for freq=%12.8e\n", half_fc/OCT_CUBEROOT_TWO);
         free(sound_data);
         exit(1);
     }
     fprintf(stdout, "upper freq=%12.4e, center_freq=%12.4e, lower_freq=%12.4e\n", half_fc, half_fc/OCT_CUBEROOT_TWO, half_fc/OCT_CUBEROOT_TWO_SQ);
-    if (one_third_filter(n, half_fc/OCT_CUBEROOT_TWO_SQ, bl, al) == OCT_ERROR_CODE) {
+    if (one_third_filter(n, half_fc/OCT_CUBEROOT_TWO_SQ, fs, bl, al) == OCT_ERROR_CODE) {
         fprintf(stderr, "Failed to design lower octave 1/3 filter for freq=%12.8e\n", half_fc/OCT_CUBEROOT_TWO_SQ);
         free(sound_data);
+        fclose(fid);
         exit(1);
     }
     // FIR filter used for decimation
@@ -355,6 +325,7 @@ int main(int argc, char*argv[]) {
             fprintf(stderr, "Cannot resize input after decimation\n");
             free(decimated_out);
             firdecim_rrrf_destroy(decim);
+            fclose(fid);
             exit(1);
         }
         memcpy(sound_data, decimated_out, decimated_size * sizeof(float));
@@ -365,6 +336,7 @@ int main(int argc, char*argv[]) {
                 free(decimated_out);
                 firdecim_rrrf_destroy(decim);
                 free(decimated_out);
+                fclose(fid);
                 exit(1);
             }
         }
@@ -431,10 +403,14 @@ int main(int argc, char*argv[]) {
         iirfilt_rrrf_destroy(octave_one_third_center);
         iirfilt_rrrf_destroy(octave_one_third_lower);
     }
-
+    // Add the followin the output GNU octave file
+    //>> axis([0 31 0 40])
+    //>> set(gca, 'XTickLabel', F)
+    //>> bar(rms_power)
     // Cleanup owned resources by the program
     firdecim_rrrf_destroy(decim);
     fprintf(stdout, "Debug: Liquid resources cleared\n");
     free(sound_data);
+    fclose(fid);
     return 0;
 }
